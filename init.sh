a68795e47c92057930eab972f1b63bd0151bbce1
milx <david.hacker09@gmail.com>
2023-05-14 17:24:16 +0330

    added init script for local-dev

--- init.sh
+++ init.sh
@@ -1,1 +1,21 @@
 #!/bin/bash
+
+# Define the list of microservices in the monorepo
+MICROSERVICES=("microservice1" "microservice2" "microservice3" ... "microservice10")
+
+# Loop through each microservice and generate the Kubernetes YAML files
+for MICROSERVICE in "${MICROSERVICES[@]}"
+do
+  # Generate the Kubernetes deployment YAML file for the microservice
+  cd $MICROSERVICE
+  docker build -t $MICROSERVICE .
+  docker push $MICROSERVICE
+  cat deployment.yaml.template | sed "s/{{MICROSERVICE}}/$MICROSERVICE/g" > deployment.yaml
+
+  # Generate the Kubernetes service YAML file for the microservice
+  cat service.yaml.template | sed "s/{{MICROSERVICE}}/$MICROSERVICE/g" > service.yaml
+
+  # Deploy the microservice to Kubernetes
+  kubectl apply -f deployment.yaml
+  kubectl apply -f service.yaml
+done
