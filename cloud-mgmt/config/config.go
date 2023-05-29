9c07cb32d81dee25b816ffc1a3c4cb6b1d1df1ef
milx <david.hacker09@gmail.com>
2023-05-29 17:38:04 +0330

    refactored project entry point.

--- cloud-mgmt/config/config.go
+++ cloud-mgmt/config/config.go
@@ -1,1 +1,45 @@
 package config
+
+import (
+	"github.com/spf13/viper"
+)
+
+type Config struct {
+	HttpAddr      string      `json:"httpAddr"`
+	GrpcAddr      string      `json:"grpcAddr"`
+	MongoURI      string      `json:"mongoURI"`
+	Collections   interface{} `json:"collections"`
+	KafkaBroker   string      `json:"kafkaBroker"`
+	WebHookURL    string      `json:"webHookURL"`
+	WebHookSecret string      `json:"webHookSecret"`
+	ClientID      string      `json:"clientID"`
+	ClientSecret  string      `json:"clientSecret"`
+}
+
+func ReadConfig() (*Config, error) {
+	viper.SetConfigName("config") // name of config file (without extension)
+	viper.SetConfigType("toml")
+	viper.AddConfigPath(".") // optionally look for config in the working directory
+	//viper.AddConfigPath("/app/")
+	err := viper.ReadInConfig() // Find and read the config file
+
+	if err != nil { // Handle errors reading the config file
+		return nil, err
+	}
+
+	m := &Config{
+		HttpAddr:      viper.GetString("app.httpAddr"),
+		GrpcAddr:      viper.GetString("app.grpcAddr"),
+		MongoURI:      viper.GetString("database.mongoURI"),
+		Collections:   viper.Get("database.collections"),
+		KafkaBroker:   viper.GetString("watermill.kafkaBroker"),
+		WebHookURL:    viper.GetString("github.webHookURL"),
+		WebHookSecret: viper.GetString("github.webHookSecret"),
+		ClientID:      viper.GetString("github.auth.clientID"),
+		ClientSecret:  viper.GetString("github.auth.clientSecret"),
+	}
+
+	//fmt.Println("owner name", viper.GetString("owner.name"))
+	//fmt.Println("database user", viper.GetString("database.user"))
+	return m, nil
+}
--- cloud-mgmt/gapi/cloudmgmt-server.go
+++ cloud-mgmt/gapi/cloudmgmt-server.go
@@ -1,27 +1,23 @@
 package gapi
 
-//import (
-//	//"github.com/miladhzzzz/milx-cloud-init/api-gateway/services"
-//	"github.com/miladhzzzz/milx-cloud-init/cloud-mgmt/config"
-//	pb "github.com/miladhzzzz/milx-cloud-init/cloud-mgmt/proto"
-//	//"github.com/miladhzzzz/milx-cloud-init/cloud-mgmt/services"
-//	"go.mongodb.org/mongo-driver/mongo"
-//)
-//
-//type CloudServer struct {
-//	pb.CloudMgmtServiceServer
-//	config         config
-//	authService    services
-//	userCollection *mongo.Collection
-//}
-//
-//func NewGrpcCloudServer(config config.Config, authService services.AuthService, userCollection *mongo.Collection) (*CloudServer, error) {
-//
-//	cloudServer := &CloudServer{
-//		config:         config,
-//		authService:    authService,
-//		userCollection: userCollection,
-//	}
-//
-//	return cloudServer, nil
-//}
+import (
+	"github.com/persys-dev/persys-devops/cloud-mgmt/config"
+	pb "github.com/persys-dev/persys-devops/cloud-mgmt/proto"
+	"github.com/persys-dev/persys-devops/cloud-mgmt/services"
+)
+
+type CloudServer struct {
+	pb.CloudMgmtServiceServer
+	config       config.Config
+	cloudService services.CloudService
+}
+
+func NewGrpcCloudServer(config config.Config, cloudService services.CloudService) (*CloudServer, error) {
+
+	cloudServer := &CloudServer{
+		config:       config,
+		cloudService: cloudService,
+	}
+
+	return cloudServer, nil
+}
