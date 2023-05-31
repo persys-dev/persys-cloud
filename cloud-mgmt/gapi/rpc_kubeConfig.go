9f04fe006f44538eca49c7ede7df5677c1b76756
milx <david.hacker09@gmail.com>
2023-05-31 11:17:05 +0330

    RPC service for cloud manager.

--- cloud-mgmt/gapi/rpc_kubeConfig.go
+++ cloud-mgmt/gapi/rpc_kubeConfig.go
@@ -1,1 +1,13 @@
 package gapi
+
+import (
+	"context"
+	pb "github.com/persys-dev/persys-devops/cloud-mgmt/proto"
+)
+
+func (cloud *CloudServer) kubeConfig(ctx context.Context, req *pb.ServicesRequest) (*pb.ServicesResponse, error) {
+	// Use services
+	//cloud.cloudService
+
+	return nil, nil
+}
