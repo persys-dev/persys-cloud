972ee01b06f45710fdf915ab40888aba31152e95
milx <david.hacker09@gmail.com>
2026-02-18 00:09:07 +0330

    Chore: Remove Old files

--- cloud-mgmt/services/cloud.service.go
+++
@@ -1,8 +0,0 @@
-package services
-
-//import "github.com/persys-dev/persys-devops/cloud-mgmt/models"
-
-type CloudService interface {
-	//SignUpUser(*models.SignUpInput) (*models.DBResponse, error)
-	//SignInUser(*models.SignInInput) (*models.DBResponse, error)
-}
--- prow/internal/models/auth.go
+++
@@ -1,7 +0,0 @@
-package models
-
-// AuthConfig holds authentication-related configuration for nodes.
-type AuthConfig struct {
-	PublicKey  string `json:"publicKey"`  // Hex-encoded RSA public key
-	SharedSecret string `json:"sharedSecret"` // Optional shared secret for Secret Mode
-}
